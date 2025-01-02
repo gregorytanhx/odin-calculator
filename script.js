b39e46af9b38c814d4302c70af8ce1687bcb1aea
gregorytanhx <48156960+gregorytanhx@users.noreply.github.com>
2025-01-02 22:16:33 +0800

    numbers now appear on display

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function divide(a, b) {
 }   
 
 function operate(a, b, operator) {
+    a = Number(a);
+    b = Number(b); 
     switch(operator) {
         case '+':
             return add(a, b);
@@ -28,7 +30,58 @@ function operate(a, b, operator) {
     }
 }
 
+function numInput(event) {
+    let num;
+    switch(event.srcElement.id) {
+        case "zero":
+            currNum += "0";
+            break;
+        case "one":
+           currNum += "1"; 
+           break;
+        case "two":
+            currNum += "2"; 
+            break;
+        case "three":
+            currNum += "3";
+            break;
+        case "four":
+            currNum += "4";
+            break;
+        case "five":
+            currNum += "5";
+            break;
+        case "six":
+            currNum += "6";
+            break;
+        case "seven":
+            currNum += "7";
+            break;
+        case "eight":
+            currNum += "8";
+            break;
+        case "nine":
+            currNum += "9";
+            break;
+        case "decimal":
+            currNum += ".";
+            break;
+    }
+    document.querySelector(".screen").innerHTML = currNum;
+}
 
+var equation = new Array();
+var currNum = "";
+
+let numBtns = document.getElementsByClassName("num-btn");
+for (btn of numBtns) {
+    btn.addEventListener("click", numInput);
+};
+
+let operatorBtns = document.getElementsByClassName("operator-btn");
+let clearBtn = document.getElementById("clear-btn");
+let allClearBtn = document.getElementById("all-clear-btn");
+let modBtn = document.getElementById("mod-btn");
 
 
 
